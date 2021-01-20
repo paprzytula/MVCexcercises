952b10e4f4f997be6ab48e34d4b2c235458f4aad
Paweł <paprzytula@gmail.com>
2021-01-20 13:40:43 +0100

    Added CRUD IActionResults for Categories in admin area, had to add imports and viewstart in area

---
+++ SimpleProjectMVC31/SimpleProjectMVC31/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SimpleProjectMVC31.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleProjectMVC31.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        private ShopContext context;
+        public CategoryController(ShopContext context)
+        {
+            this.context = context;
+        }
+        public IActionResult Index()
+        {
+            return RedirectToAction("List", "Category");
+        }
+        [Route("[area]/Categories/{id?}")]
+        public IActionResult List(string id = "All")
+
+        {
+            var categories = context.Categories.OrderBy(c => c.CategoryId).ToList();
+            return View(categories);
+        }
+        [HttpGet]
+        public IActionResult Add()
+        {
+            ViewBag.Action = "Add";
+            return View("AddUpdate", new Category());
+        }
+        [HttpGet]
+        public IActionResult Update(int id)
+        {
+            ViewBag.Action = "Update";
+            var category = context.Categories.Find(id);
+            return View("AddUpdate", category);
+        }
+        [HttpPost]
+        public IActionResult Update(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                if (category.CategoryId == 0)
+                {
+                    context.Categories.Add(category);
+                }
+                else
+                {
+                    context.Categories.Update(category);
+                }
+                context.SaveChanges();
+                return RedirectToAction("List", "Category");
+            }
+            else
+            {
+                ViewBag.Action = "Save";
+                return View("AddUpdate");
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var category = context.Categories.Find(id);
+            return View(category);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Category category)
+        {
+            context.Categories.Remove(category);
+            context.SaveChanges();
+            return RedirectToAction("List", "Category");
+        }
+    }
+}
--- SimpleProjectMVC31/SimpleProjectMVC31/Startup.cs
+++ SimpleProjectMVC31/SimpleProjectMVC31/Startup.cs
@@ -36,6 +36,11 @@ namespace SimpleProjectMVC31
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapAreaControllerRoute(
+                    name: "admin",
+                    areaName:"Admin",
+                    pattern: "Admin/{controller=Home}/{action=Index}/{id?}");
+
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}/{slug?}");
