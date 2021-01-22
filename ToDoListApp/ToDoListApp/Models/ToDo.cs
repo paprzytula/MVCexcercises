68c34afd9f43abba945eb9db40765e71de258a83
Paweł <paprzytula@gmail.com>
2021-01-22 21:14:18 +0100

    Todo list with dropdown, multiple filters to apply, classes in a View

---
+++ ToDoListApp/ToDoListApp/Models/ToDo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoListApp.Models
+{
+    public class ToDo
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage ="Please enter description")]
+        public string Description { get; set; }
+        [Required(ErrorMessage ="Please enter the due date")]
+        public DateTime? DueDate { get; set; }
+        [Required(ErrorMessage ="Please select the category")]
+        public string CategoryId { get; set; }
+        public Category Category { get; set; }
+        [Required(ErrorMessage = "Please select the status")]
+        public string StatusId { get; set; }
+        public Status Status { get; set; }
+
+        public bool Overdue => StatusId == "open" && DueDate < DateTime.Today;
+
+    }
+}
---
+++ ToDoListApp/ToDoListApp/Models/ToDoContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ToDoListApp.Models
+{
+    public class ToDoContext : DbContext
+    {
+        public ToDoContext(DbContextOptions<ToDoContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<ToDo> ToDos { get; set; }
+        public DbSet<Status> Statuses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Category>().HasData(
+new Category { CategoryId = "work", Name = "Work" },
+new Category { CategoryId = "home", Name = "Home" },
+new Category { CategoryId = "ex", Name = "Exercise" },
+new Category { CategoryId = "shop", Name = "Shopping" },
+new Category { CategoryId = "contact", Name = "Contact" }
+                );
+            modelBuilder.Entity<Status>().HasData(
+                new Status { StatusId = "open", Name = "Open" },
+                new Status { StatusId = "completed", Name = "Completed" }
+                );
+        }
+    }
+}
